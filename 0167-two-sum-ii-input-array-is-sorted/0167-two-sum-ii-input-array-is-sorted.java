b807c427356eb9e958f25b47eb5ed32c489275a9
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2025-03-12 23:17:06 +0530

    Time: 2 ms (92.71%), Space: 47.2 MB (50.97%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,38 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
+    public int[] twoSum_(int[] numbers, int target) {
         int[] arr = new int[2];
         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-        
-        for(int i=0; i< numbers.length; i++){
-            if(!hm.containsKey(target-numbers[i])){
-                hm.put(numbers[i], i+1 );
-            }else{
-                arr[0] = hm.get(target-numbers[i]);
-                arr[1] = i+1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (!hm.containsKey(target - numbers[i])) {
+                hm.put(numbers[i], i + 1);
+            } else {
+                arr[0] = hm.get(target - numbers[i]);
+                arr[1] = i + 1;
                 return arr;
             }
         }
-        
+
         return null;
+    }
+
+    // Solution with two pointers and constant space
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int[] result = new int[2];
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                result[0] = left+1;
+                result[1] = right+1;
+                return result;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
     }
 }
