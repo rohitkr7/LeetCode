e3b8ac9df84bf3ec63d56101dd4436ffb8f633b6
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2025-03-14 23:34:58 +0530

    Time: 24 ms (74.7%), Space: 46.7 MB (43.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,6 +1,8 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
+        // Initial Zero Sum: The initial entry {0: 1} ensures that any subarray starting
+        // from the beginning of the array and summing to k is counted.
         map.put(0, 1); // Initialize with sum 0 at index -1
         int count = 0;
         int sum = 0;
