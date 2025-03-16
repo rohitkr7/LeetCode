206d7710eabc3fdc523ec5b405ca64f1e37491e9
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2025-03-16 12:05:04 +0530

    Time: 2 ms (87.37%), Space: 45.1 MB (38.59%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,87 @@
+class Solution {
+
+    // Solution Explaination from:
+    // https://takeuforward.org/data-structure/n-queen-problem-return-all-distinct-solutions-to-the-n-queens-puzzle/
+
+    // Time Complexity: Exponential in nature since we are trying out all ways, to
+    // be precise its O(N! * N)
+    // Space Complexity: O( N2 )
+
+    public static List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> res = new ArrayList<List<String>>();
+        dfs(0, board, res);
+        return res;
+    }
+
+    static boolean isSafe(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q')
+                return false;
+            col--;
+            row++;
+        }
+        return true;
+    }
+
+    static void dfs(int col, char[][] board, List<List<String>> res) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                dfs(col + 1, board, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+
+    public static void main(String args[]) {
+        int N = 4;
+        List<List<String>> queen = solveNQueens(N);
+        int i = 1;
+        for (List<String> it : queen) {
+            System.out.println("Arrangement " + i);
+            for (String s : it) {
+                System.out.println(s);
+            }
+            System.out.println();
+            i += 1;
+        }
+
+    }
+}
