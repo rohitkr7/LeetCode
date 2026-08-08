21ecad52e28cdaeb6ffa6fb9fe673731058b2bc3
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-08 15:01:24 +0530

    Time: 0 ms (100%), Space: 46.7 MB (73.38%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,7 +10,10 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
+
+    // Time: O(n)
+    // Space: O(n)
+    public boolean hasCycle_UsingHashSet(ListNode head) {
         HashSet<ListNode> hs = new HashSet<ListNode>();
         ListNode node = head;
         while(node!=null){
@@ -20,5 +23,20 @@ public class Solution {
             node = node.next;
         }
         return false;
+    }
+
+    // Time: O(n)
+    // Space: O(1)
+    public boolean hasCycle(ListNode head) {
+        if(head ==null || head.next == null)
+            return false;
+        ListNode slow = head, fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null)
+                return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 }
